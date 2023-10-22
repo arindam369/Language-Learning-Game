c302da7daeb3b75b444436d3e41259174bbfc4f7
Arindam Halder <halderarindam10000@gmail.com>
2023-10-22 19:16:14 +0530

    :bug: mongoose url fixed
    mongoose url fixed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,6 +5,6 @@ export const connectMongoDB = async ()=>{
   if(mongoose.connection.readyState === 1){
     return mongoose.connection.asPromise();
   }
-  return await mongoose.connect(process.env.MONGO_URL);
-  // return await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.hspwrdh.mongodb.net/language-api?retryWrites=true&w=majority`);
+  // return await mongoose.connect(process.env.MONGO_URL);
+  return await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.hspwrdh.mongodb.net/language-api?retryWrites=true&w=majority`);
 }
